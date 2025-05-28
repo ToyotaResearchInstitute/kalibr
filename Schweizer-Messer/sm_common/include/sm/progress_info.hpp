667ce0b716bede8a7bf776fa42696793633a0d17
Francisco Rossi <frossi@ekumenlabs.com>
2025-05-28 11:50:11 -0300

    Migrate sm_common to pure CMake

--- Schweizer-Messer/sm_common/include/sm/progress_info.hpp
+++ Schweizer-Messer/sm_common/include/sm/progress_info.hpp
@@ -3,14 +3,30 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <iostream>     // std::cout, std::fixed
+#include <iomanip>      // std::setprecision
 
 #include <sm/assert_macros.hpp>
 
 namespace sm {
 
-	void showProgress(double progress);
+    void showProgress(double progress)
+    {
+        SM_ASSERT_GE_DBG(std::runtime_error, progress, 0.0, "negative progress! shame on you!");
+        SM_ASSERT_LE_DBG(std::runtime_error, progress, 1.0, "progress out of bounds!");
 
-    template<typename T1, typename T2> 
+        std::cout << std::fixed << std::setprecision(2) << std::showpoint;
+        if(progress >= 1.0)
+        {
+            std::cout << "\r" << "Complete!             " << std::endl;
+        } else
+        {
+            std::cout << "\r" << 100 * progress << "% complete ";
+            std::cout.flush();
+        }
+    }
+
+    template<typename T1, typename T2>
     void showProgress(T1 done, T2 all)
     {
         SM_ASSERT_GT(std::runtime_error, all, 0, "#DIV0");
