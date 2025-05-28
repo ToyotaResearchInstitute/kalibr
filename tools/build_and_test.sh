e7e96524d5a579ad19082080884e8f192d7132b8
Francisco Rossi <50388097+frneer@users.noreply.github.com>
2025-05-28 10:53:30 -0300

    Add basic docker infra and CI workflow (#1)
    This patch adds the basic docker infrastructure to build the project and
    creates a CI workflow for the repo that builds and tests the packages.

---
+++ tools/build_and_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Currently we build and test the packages already migrated to ROS2 / Ubuntu noble.
+packages=(
+    "" \
+)
+
+echo "=== Building packages ==="
+colcon build --packages-select ${packages[@]}
+
+echo "=== Build complete. Starting tests ==="
+colcon test --packages-select ${packages[@]}
+
+echo "=== Tests complete. Showing results ==="
+colcon test-result --verbose
