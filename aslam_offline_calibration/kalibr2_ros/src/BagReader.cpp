3783def08522e4b63c6fae82381448906c81e1a2
Francisco Rossi <frossi@ekumenlabs.com>
2025-07-09 16:47:35 -0300

    Add missing namespaces

--- aslam_offline_calibration/kalibr2_ros/src/BagReader.cpp
+++ aslam_offline_calibration/kalibr2_ros/src/BagReader.cpp
@@ -1,6 +1,10 @@
 #include "kalibr2_ros/BagReader.hpp"
 
 
+namespace kalibr2 {
+
+namespace ros {
+
 rosbag2_storage::BagMetadata get_bag_metadata(const std::string& bag_file_path) {
   auto metadata_directory = std::filesystem::path(bag_file_path).parent_path();
   auto metadata_io = rosbag2_storage::MetadataIo();
@@ -15,7 +19,8 @@ rosbag2_storage::StorageOptions get_storage_options(const std::string& bag_file_
   return storage_options;
 }
 
-std::unique_ptr<ImageReader> create_bag_reader(const std::string& bag_file_path, const std::string& topic) {
+
+std::unique_ptr<kalibr2::ImageReader> create_bag_reader(const std::string& bag_file_path, const std::string& topic) {
   auto bag_metadata = get_bag_metadata(bag_file_path);
   auto storage_options = get_storage_options(bag_file_path, bag_metadata);
   // Assuming the serialization format is CDR
@@ -33,12 +38,16 @@ std::unique_ptr<ImageReader> create_bag_reader(const std::string& bag_file_path,
   for (const auto& topic_metadata : reader->get_all_topics_and_types()) {
     if (topic_metadata.name == topic) {
       if (topic_metadata.type == "sensor_msgs/msg/Image") {
-        return std::make_unique<BagImageReader<sensor_msgs::msg::Image>>(std::move(reader));
+        return std::make_unique<kalibr2::ros::BagImageReader<sensor_msgs::msg::Image>>(std::move(reader));
       } else if (topic_metadata.type == "sensor_msgs/msg/CompressedImage") {
-        return std::make_unique<BagImageReader<sensor_msgs::msg::CompressedImage>>(std::move(reader));
+        return std::make_unique<kalibr2::ros::BagImageReader<sensor_msgs::msg::CompressedImage>>(std::move(reader));
       } else {
         throw std::runtime_error("Unsupported image type: " + topic_metadata.type);
       }
     }
   }
 }
+
+} // namespace ros
+
+} // namespace kalibr2
--- aslam_offline_calibration/kalibr2_ros/test/BagReaderTest.cpp
+++ aslam_offline_calibration/kalibr2_ros/test/BagReaderTest.cpp
@@ -9,13 +9,14 @@
 #include <aslam/cameras/GridDetector.hpp>
 #include <aslam/cameras/GridCalibrationTargetAprilgrid.hpp>
 
+namespace {
 
 TEST(BagReaderTest, CanInitializeBagReader) {
   std::string bag_path = std::string(TEST_DATA_DIR) + "/rosbag2_2025_06_11-12_00_21_0.mcap";
   if (!std::filesystem::exists(bag_path)) {
     GTEST_SKIP() << "Bag file does not exist: " << bag_path;
   }
-  auto reader = create_bag_reader(bag_path, "/BFS_25037070/image");
+  auto reader = kalibr2::ros::create_bag_reader(bag_path, "/BFS_25037070/image");
 }
 
 
@@ -24,8 +25,8 @@ TEST(BagReaderTest, CanReadSingleImageSingleTopic) {
   if (!std::filesystem::exists(bag_path)) {
     GTEST_SKIP() << "Bag file does not exist: " << bag_path;
   }
-  auto reader = create_bag_reader(bag_path, "/BFS_25037070/image");
-  Image img = reader->ReadNext();
+  auto reader = kalibr2::ros::create_bag_reader(bag_path, "/BFS_25037070/image");
+  kalibr2::Image img = reader->ReadNext();
 }
 
 
@@ -34,11 +35,11 @@ TEST(DISABLED_BagReaderTest, CanReadMultipleImagesSingleTopic) {
   if (!std::filesystem::exists(bag_path)) {
     GTEST_SKIP() << "Bag file does not exist: " << bag_path;
   }
-  auto reader = create_bag_reader(bag_path, "/BFS_25037070/image");
+  auto reader = kalibr2::ros::create_bag_reader(bag_path, "/BFS_25037070/image");
   cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
 
   while (reader->HasNext()) {
-    Image img = reader->ReadNext();
+    kalibr2::Image img = reader->ReadNext();
     assert (!img.image.empty());
     cv::imshow("Image", img.image);
     cv::waitKey(1);
@@ -56,9 +57,9 @@ TEST(DISABLED_BagReaderTest, CanReadMultipleImagesMultipleTopics)
   cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
 
   for (const auto& topic : topics) {
-    auto reader = create_bag_reader(bag_path, topic);
+    auto reader = kalibr2::ros::create_bag_reader(bag_path, topic);
     while (reader->HasNext()) {
-      Image img = reader->ReadNext();
+      kalibr2::Image img = reader->ReadNext();
       assert (!img.image.empty());
       cv::imshow("Image", img.image);
       cv::waitKey(1);
@@ -72,7 +73,7 @@ TEST(DISABLED_BagReaderTest, CanDetectMultipleImagesSingleTopic) {
   if (!std::filesystem::exists(bag_path)) {
     GTEST_SKIP() << "Bag file does not exist: " << bag_path;
   }
-  auto reader = create_bag_reader(bag_path, "/BFS_25037070/image");
+  auto reader = kalibr2::ros::create_bag_reader(bag_path, "/BFS_25037070/image");
   cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
 
   auto target_grid = boost::make_shared<aslam::cameras::GridCalibrationTargetAprilgrid>(5, 6, 0.088, 0.2954);
@@ -80,12 +81,12 @@ TEST(DISABLED_BagReaderTest, CanDetectMultipleImagesSingleTopic) {
   auto detector = aslam::cameras::GridDetector(geometry, target_grid);
 
   while (reader->HasNext()) {
-    Image img = reader->ReadNext();
+    kalibr2::Image img = reader->ReadNext();
     assert (!img.image.empty());
     cv::imshow("Image", img.image);
     cv::waitKey(1);
 
-    auto observation = ToObservation(img, detector);
+    auto observation = kalibr2::ToObservation(img, detector);
 
     if (observation.has_value()) {
       std::cout << "Found target in image with timestamp: " << img.timestamp.toNSec() << std::endl;
@@ -94,4 +95,6 @@ TEST(DISABLED_BagReaderTest, CanDetectMultipleImagesSingleTopic) {
       std::cout << "No target found in image with timestamp: " << img.timestamp.toNSec() << std::endl;
     }
   }
+}
+
 }
