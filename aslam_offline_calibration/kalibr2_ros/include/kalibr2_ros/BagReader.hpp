47068854889f8763cf25ac6ea04549c18eeb04c4
Francisco Rossi <frossi@ekumenlabs.com>
2025-07-09 16:47:35 -0300

    Add missing namespaces

--- aslam_offline_calibration/kalibr2_ros/include/kalibr2_ros/BagReader.hpp
+++ aslam_offline_calibration/kalibr2_ros/include/kalibr2_ros/BagReader.hpp
@@ -16,10 +16,13 @@
 #include <aslam/Time.hpp>
 #include <kalibr2/Image.hpp>
 
+namespace kalibr2 {
+
+namespace ros {
 
 template<typename MessageT>
 Image image_from_message(const MessageT& msg) {
-  Image img;
+  kalibr2::Image img;
   img.timestamp = aslam::Time(msg.header.stamp.sec, msg.header.stamp.nanosec);
   img.image = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8)->image;
 
@@ -28,11 +31,11 @@ Image image_from_message(const MessageT& msg) {
 
 
 template<typename MessageT>
-class BagImageReader : public ImageReader {
+class BagImageReader : public kalibr2::ImageReader {
   public:
   BagImageReader(std::unique_ptr<rosbag2_cpp::Reader> reader) : reader_(std::move(reader)) {}
 
-  Image ReadNext() override {
+  kalibr2::Image ReadNext() override {
     auto msg = reader_->read_next<MessageT>();
     return image_from_message(msg);
   }
@@ -53,3 +56,6 @@ rosbag2_storage::StorageOptions get_storage_options(const std::string& bag_file_
 
 std::unique_ptr<ImageReader> create_bag_reader(const std::string& bag_file_path, const std::string& topic);
 
+} // namespace ros
+
+} // namespace kalibr2
