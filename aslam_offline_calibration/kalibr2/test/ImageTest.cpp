b48f7bd64b5f4afc58a6c8e0511c9bfc4bc7d89f
Francisco Rossi <frossi@ekumenlabs.com>
2025-07-09 12:55:40 -0300

    Add missing namespaces

--- aslam_offline_calibration/kalibr2/test/ImageTest.cpp
+++ aslam_offline_calibration/kalibr2/test/ImageTest.cpp
@@ -10,9 +10,11 @@
 #include <kalibr2/CameraModels.hpp>
 
 
+namespace {
+
 TEST(ImageTest, ToObservationNoTarget) {
     // Black image emulating no target
-    Image img;
+    kalibr2::Image img;
     img.timestamp = aslam::Time(1622547800, 0);
     img.image = cv::Mat::zeros(480, 640, CV_8UC3);
 
@@ -20,13 +22,13 @@ TEST(ImageTest, ToObservationNoTarget) {
     auto geometry = boost::make_shared<kalibr2::models::DistortedPinhole::Geometry>();
     auto detector = aslam::cameras::GridDetector(geometry, target_grid);
 
-    auto observation = ToObservation(img, detector);
+    auto observation = kalibr2::ToObservation(img, detector);
 
     ASSERT_FALSE(observation.has_value());
 }
 
 TEST(ImageTest, ToObservationCircleGridTarget) {
-    Image img;
+    kalibr2::Image img;
     img.timestamp = aslam::Time(1622547800, 0);
     img.image = cv::imread("data/testImageCircleGrid.jpg", cv::IMREAD_GRAYSCALE);
     ASSERT_FALSE(img.image.empty());
@@ -35,7 +37,9 @@ TEST(ImageTest, ToObservationCircleGridTarget) {
     auto geometry = boost::make_shared<kalibr2::models::DistortedPinhole::Geometry>();
     auto detector = aslam::cameras::GridDetector(geometry, target_grid);
 
-    auto observation = ToObservation(img, detector);
+    auto observation = kalibr2::ToObservation(img, detector);
 
     ASSERT_TRUE(observation.has_value());
+}
+
 }
