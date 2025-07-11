e4b698e926fb8bdf5a615fccca110aa70d7828b3
Francisco Rossi <frossi@ekumenlabs.com>
2025-07-11 13:58:13 -0300

    Run linter pass

--- aslam_offline_calibration/kalibr2/include/kalibr2/Image.hpp
+++ aslam_offline_calibration/kalibr2/include/kalibr2/Image.hpp
@@ -3,10 +3,9 @@
 #include <optional>
 
 #include <aslam/Time.hpp>
-#include <opencv2/core.hpp>
-
 #include <aslam/cameras/GridCalibrationTargetObservation.hpp>
 #include <aslam/cameras/GridDetector.hpp>
+#include <opencv2/core.hpp>
 
 namespace kalibr2 {
 
@@ -15,14 +14,19 @@ struct Image {
   cv::Mat image;
 };
 
-/// Transform an Image into a GridCalibrationTargetObservation using the provided GridDetector.
+/// Transform an Image into a GridCalibrationTargetObservation using the
+/// provided GridDetector.
 /* Aims to translate extractCornersFromDataset python function
 https://github.com/ToyotaResearchInstitute/kalibr/blob/c9433f9b4bbf9d99f890c480e29a34f04a3c6af8/aslam_offline_calibration/kalibr/python/kalibr_common/TargetExtractor.py#L34
 To C++. With some simplifications:
-  1. No support for parallel processing of the images (dropped because for the moment we can only read images sequentially)
-  2. We don't use the findTarget with transformation, since it's not being used in kalibr_calibrate_cameras code which is the part we currently care about.
-  3. We always clear the image in the observation to save space, no reason to keep it around.
+  1. No support for parallel processing of the images (dropped because for the
+moment we can only read images sequentially)
+  2. We don't use the findTarget with transformation, since it's not being used
+in kalibr_calibrate_cameras code which is the part we currently care about.
+  3. We always clear the image in the observation to save space, no reason to
+keep it around.
 */
-std::optional<aslam::cameras::GridCalibrationTargetObservation> ToObservation(const Image& image, const aslam::cameras::GridDetector& detector);
+std::optional<aslam::cameras::GridCalibrationTargetObservation> ToObservation(
+    const Image& image, const aslam::cameras::GridDetector& detector);
 
-}
+}  // namespace kalibr2
--- aslam_offline_calibration/kalibr2/src/Image.cpp
+++ aslam_offline_calibration/kalibr2/src/Image.cpp
@@ -2,9 +2,12 @@
 
 namespace kalibr2 {
 
-std::optional<aslam::cameras::GridCalibrationTargetObservation> ToObservation(const Image& image, const aslam::cameras::GridDetector& detector) {
-  auto observation = aslam::cameras::GridCalibrationTargetObservation(detector.target());
-  bool success = detector.findTargetNoTransformation(image.image, image.timestamp, observation);
+std::optional<aslam::cameras::GridCalibrationTargetObservation> ToObservation(
+    const Image& image, const aslam::cameras::GridDetector& detector) {
+  auto observation =
+      aslam::cameras::GridCalibrationTargetObservation(detector.target());
+  bool success = detector.findTargetNoTransformation(
+      image.image, image.timestamp, observation);
 
   // Delete Image copy
   observation.clearImage();
@@ -12,4 +15,4 @@ std::optional<aslam::cameras::GridCalibrationTargetObservation> ToObservation(co
   return success ? std::make_optional(observation) : std::nullopt;
 }
 
-}
+}  // namespace kalibr2
